57101247b22dc4689b083b21867040963110bce7
Anne Castrillon <anne.castrillon@gmail.com>
2023-06-27 19:31:27 -0700

    committing what i have for now

--- wasm_emulator/src/lib.rs
+++ wasm_emulator/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use nes_emulator::cpu::ppu::joypad::ButtonStatus;
 use wasm_bindgen::prelude::*;
 
 extern crate nes_emulator;
@@ -8,15 +11,41 @@ use nes_emulator::cartridge::Cartridge;
 
 #[wasm_bindgen]
 pub struct WasmEmulator {
-  cpu: CPU
+  cpu: CPU,
+  key_map: HashMap<ButtonEvent, ButtonStatus>
+}
+
+#[derive(PartialEq, Eq, Hash)]
+#[wasm_bindgen]
+pub enum ButtonEvent {
+  ButtonA,
+  ButtonB,
+  Select,
+  Start,
+  Up,
+  Down,
+  Left,
+  Right
 }
 
 #[wasm_bindgen]
 impl WasmEmulator {
   #[wasm_bindgen(constructor)]
   pub fn new() -> Self {
+    let mut key_map = HashMap::new();
+
+    key_map.insert(ButtonEvent::ButtonA, ButtonStatus::BUTTON_A);
+    key_map.insert(ButtonEvent::ButtonB, ButtonStatus::BUTTON_B);
+    key_map.insert(ButtonEvent::Select, ButtonStatus::SELECT);
+    key_map.insert(ButtonEvent::Start, ButtonStatus::START);
+    key_map.insert(ButtonEvent::Up, ButtonStatus::UP);
+    key_map.insert(ButtonEvent::Down, ButtonStatus::DOWN);
+    key_map.insert(ButtonEvent::Left, ButtonStatus::LEFT);
+    key_map.insert(ButtonEvent::Right, ButtonStatus::RIGHT);
+
     WasmEmulator {
-      cpu: CPU::new()
+      cpu: CPU::new(),
+      key_map
     }
   }
 
@@ -35,5 +64,11 @@ impl WasmEmulator {
   pub fn load(&mut self, rom: &[u8]) {
     let cartridge = Cartridge::new(rom.to_vec());
     self.cpu.load_game(cartridge);
+  }
+
+  pub fn update_input(&mut self, button_event: ButtonEvent, is_pressed: bool) {
+    if let Some(button) = self.key_map.get(&button_event) {
+      self.cpu.ppu.joypad.set_button(*button, is_pressed);
+    }
   }
 }
